bb9c515a8bc2a324e7d9d82336d8f9cd231bfdd7
Amir Kohzadi <amir.kohzadi@gmail.com>
2025-12-23 22:55:18 +0100

    class based view!
    a demonstration of basic CRUD with the class based function with various permission mixins!

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -10,11 +10,12 @@ urlpatterns = [
     # path('', TemplateView.as_view(template_name='index.html')),
     path('',views.IndexView.as_view(),name='cbv-index'),
     path('post/',views.PostList.as_view(),name='post-list'),
-    path('post/api',views.PostListApi.as_view(),name='post-list-api'),
     path('post/<int:pk>',views.PostDetailView.as_view(),name='post-detail'),
     path('post/<int:pk>/edit',views.PostEditView.as_view(),name='post-edit'),
     path('post/<int:pk>/delete',views.PostDeleteView.as_view(),name='post-delete'),
     path('post/create',views.PostCreateView.as_view(),name='post-create'),
-    path('gomaktab',views.RedirectToMaktab.as_view(),name='redirect-maktab'),
-    path('api/v1/', include('blog.api.v1.urls'))
+    path('google',views.RedirectToGoogle.as_view(),name='redirect-google'),
+    #until now we used ClassBasedViews!now we use ApiView!#
+    # path('api/v1/', include('blog.api.v1.urls')),
+    # path('post/api',views.PostListApi.as_view(),name='post-list-api')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
-
+from django.shortcuts import (render,
+                              redirect,)
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  FormView,
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView,)
+from django.views.generic.base import (TemplateView,
+                                       RedirectView,)
+from accounts.models import Profile
+from .models import Post
+from .forms import PostFrom
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+
+
+'''
+class based views all this page!
+'''
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'AMIR'
+        return context
+
+class RedirectToGoogle(RedirectView):
+    url = 'https://google.com'
+
+class PostList(LoginRequiredMixin,ListView):
+    context_object_name = 'posts'
+    def  get_queryset(self):
+        posts = Post.objects.all()
+        return posts
+    
+class PostListApi(TemplateView):
+    template_name = 'blog/post_list_api.html'
+    
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    form_class = PostFrom
+    success_url = '/blog/post/'
+
+    def form_valid(self, form):
+        form.instance.author = Profile.objects.get(user=self.request.user)
+        return super().form_valid(form)
+    
+class PostEditView(LoginRequiredMixin,UpdateView):
+    model = Post
+    form_class = PostFrom
+    success_url = '/blog/post/'
+    #we are creating this filter in order that only the user owner or admin can edit a post!#
+    def get_queryset(self):
+        return Post.objects.filter(author__user=self.request.user)
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = '/blog/post/'
+    #we are creating this filter in order that only the user owner or admin can delete a post!#
+    def get_queryset(self):
+        return Post.objects.filter(author__user=self.request.user)
